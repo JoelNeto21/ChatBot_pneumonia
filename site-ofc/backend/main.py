97877ff2efa1dd244701ebcdfae81d61b0789c8f
miguelmassane <miguelmassane05@gmail.com>
2026-04-12 21:12:15 -0300

    Update 7

--- site-ofc/backend/main.py
+++ site-ofc/backend/main.py
@@ -13,6 +13,7 @@ VISION_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
 # Modelo de Texto para conversação
 TEXT_MODEL = "llama-3.3-70b-versatile"
 # Chave de API Groq
+API_KEY = ""
 # ==========================================
 
 app = FastAPI()
